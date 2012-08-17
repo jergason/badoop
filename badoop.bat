f5dd713def2542eedb2bd3c8243f6ef7f911b928
Steve Nay <steven.nay@gmail.com>
2012-08-17 14:29:15 -0600

    Add compatible Windows batch file version
    The Windows implementation mirrors the functionality of the original.
    There may be some slight regex differences with findstr vs. sed.
    Sometimes claims an error when deleting, even though the deletion
    succeeded.

---
+++ badoop.bat
@@ -0,0 +1,45 @@
+@echo off
+
+if defined TODO (
+    set BADOOP=%TODO%
+) else (
+    set BADOOP=%HOMEDRIVE%%HOMEPATH%\.todo.txt
+)
+
+:BADOOP
+if "%1" == "" (
+    :: Print out the todo file
+    if exist "%BADOOP%" (
+        type "%BADOOP%"
+    )
+) else if "%1" == "-h" (
+    goto BADOOP_USAGE
+) else if "%1" == "-d" (
+    goto BADONE
+) else (
+    :: Add a new todo
+    echo   • %* >> "%BADOOP%"
+)
+exit /B 0
+
+:BADOOP_USAGE
+echo badoop
+echo   -h: Print this help
+echo   -d: Delete any todo items matching remaining arguments
+echo   with no flags: add the string passed in to the todo list
+echo   with no arguments: print out todo list
+exit /B 0
+
+:: Badoop-done. Delete from todo list
+:BADONE
+:: NOTE: This sometimes prints "The system cannot find the drive specified."
+:: Don't know why that is, but it works anyway. It will fail line 41 of the 
+:: roundup test because of that.
+
+if exist "%BADOOP%" (
+    :: Can't do this inline like sed, so we're using a temp file
+    :: findstr complains about the -d but ignores it
+    findstr /v /r %* "%BADOOP%" 1> C:\TEMP\badooptemp.txt
+    type C:\TEMP\badooptemp.txt > "%BADOOP%"
+)
+exit /B 0
